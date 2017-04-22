e2639154e89ba9a26238e8e35dd46425bedb993c
Scott Olsen <scottolsen@Scotts-MacBook-Pro.local>
2017-04-22 11:08:57 -0400

    Began working on sh conversion from properties to json

--- sh/p2j.sh
+++ sh/p2j.sh
@@ -3,6 +3,11 @@
 VERSION=1.0.0
 PROPERTIESFILES=()
 JSONFILES=()
+CONVERT=0
+
+jsonStringify () {
+	echo "$1" | sed -E 's/^(.*)/"\1/; s/:/":"/; s/(.*)$/\1",/;' 
+}
 
 version () {
 	echo "$VERSION"
@@ -11,10 +16,31 @@ version () {
 convert () {
 	for k in "${PROPERTIESFILES[@]}"
 	do
-		echo "$k"
+		if [ ! -f $k ]; then 
+			echo "$k is not a file"
+		else
+			echo "$k is indeed a file"
+		fi
+		JTEXT="{"	
+		while read -r line
+		do
+			echo "$line"
+			rline="$(echo $line | sed s/=/:/)" 
+			echo "$rline"
+			jline="$(jsonStringify "$rline")"
+			JTEXT="$JTEXT$jline"
+		done < "$k"
+		JTEXT="$JTEXT}"
+		JTEXT="$(echo "$JTEXT" | sed s/,}/}/)"
+		output="$(echo "$k" | sed s/.properties/.json/)"
+		echo "$JTEXT"
+		echo "$output"
+		echo $JTEXT > $output		
 	done
+	
 	for i in "${JSONFILES[@]}"
 	do
+		PROPTEXT=""
 		echo "$i"
 	done
 }
@@ -25,8 +51,17 @@ do
 		--version | -v )
 			version
 			;;
+		--help | -h )
+			help
+			;;
 		--convert | -c )
-			convert
+			CONVERT=1
+			;;
+		--update | -u )
+			update
+			;;
+		--append | -a )
+			append
 			;;
 		*.properties )
 			PROPERTIESFILES=("${PROPERTIESFILES[@]}" "$1")
@@ -44,3 +79,8 @@ do
 	esac
 	shift
 done
+echo "${PROPERTIESFILES[@]}"
+echo "${JSONFILES[@]}"
+if [ $CONVERT -eq 1 ]; then
+	convert
+fi
