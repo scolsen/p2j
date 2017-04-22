d56eceb1cb362f38e7d1c1ec8589ed7456dba235
Scott Olsen <scottolsen@Scotts-MacBook-Pro.local>
2017-04-21 23:43:12 -0400

    Added file handling to sh implementation

--- sh/p2j.sh
+++ sh/p2j.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 #bash implementation of p2j
 VERSION=1.0.0
-
+PROPERTIESFILES=()
+JSONFILES=()
 
 version () {
 	echo "$VERSION"
+}
+
+convert () {
+	for k in "${PROPERTIESFILES[@]}"
+	do
+		echo "$k"
+	done
+	for i in "${JSONFILES[@]}"
+	do
+		echo "$i"
+	done
 }
 
 while test $# -gt 0
@@ -12,6 +24,15 @@ do
 	case $1 in 
 		--version | -v )
 			version
+			;;
+		--convert | -c )
+			convert
+			;;
+		*.properties )
+			PROPERTIESFILES=("${PROPERTIESFILES[@]}" "$1")
+			;;
+		*.json )
+			JSONFILES=("${JSONFILES[@]}" "$1")
 			;;
 		-* )
 			echo "Unrecognized command: $1"
